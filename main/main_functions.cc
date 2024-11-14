61646ef446fbd10d2254d081d8235c5bebbe8ff0
Ignacio <ibello@miuandes.cl>
2024-11-14 01:59:09 -0300

    esp-now

--- main/main_functions.cc
+++ main/main_functions.cc
@@ -12,10 +12,83 @@
 #include <esp_heap_caps.h>
 #include <esp_timer.h>
 #include <esp_log.h>
+#include "esp_now.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "esp_mac.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
 #include "esp_main.h"
 #include "esp_psram.h"
 
 #define INF_POWER 0.13
+#define ESP_CHANNEL 1
+
+//static uint8_t peer_mac [ESP_NOW_ETH_ALEN] = {0x3c, 0x71, 0xbf, 0xef, 0x67, 0xd0};
+static uint8_t peer_mac [ESP_NOW_ETH_ALEN] = {0xfc, 0xe8, 0xc0, 0xce, 0x53, 0xd4};
+// 3c:71:bf:ef:67:d0
+//ESP-NOW
+static const char * TAG = "esp_now_init";
+static esp_err_t init_wifi(void)
+{
+    wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+
+    esp_netif_init();
+    esp_event_loop_create_default();
+    nvs_flash_init();
+    esp_wifi_init(&wifi_init_config);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_storage(WIFI_STORAGE_FLASH);
+    esp_wifi_start();
+
+    ESP_LOGI(TAG, "wifi init completed");
+    return ESP_OK;  
+}
+
+void recv_cb(const esp_now_recv_info_t * esp_now_info, const uint8_t *data, int data_len)
+{
+    ESP_LOGI(TAG, "Data Received" MACSTR " %s", MAC2STR(esp_now_info->src_addr), data);
+}
+
+void send_cb(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    if (status == ESP_NOW_SEND_SUCCESS)
+    {
+          ESP_LOGI(TAG, "ESP_NOW_SEND_SUCCESS");
+    }
+    else{
+          ESP_LOGW(TAG, "ESP_NOW_SEND_FAIL");
+    }
+}
+
+static esp_err_t init_esp_now(void)
+{
+    esp_now_init();
+    esp_now_register_recv_cb(recv_cb);
+    esp_now_register_send_cb(send_cb);
+
+    ESP_LOGI(TAG, "esp now init completed");
+    return ESP_OK;
+}
+
+static esp_err_t register_peer(uint8_t *peer_addr)
+{
+    esp_now_peer_info_t esp_now_peer_info = {};
+    memcpy(esp_now_peer_info.peer_addr, peer_mac, ESP_NOW_ETH_ALEN);
+    esp_now_peer_info.channel = ESP_CHANNEL;
+
+    esp_now_add_peer(&esp_now_peer_info);
+    return ESP_OK;
+}
+
+static esp_err_t esp_now_send_data(const uint8_t *peer_addr, const uint8_t *data, uint8_t len)
+{
+    esp_now_send(peer_addr, data, len );
+    return ESP_OK;
+}
+
+
+
 
 namespace {
 const tflite::Model* model = nullptr;
@@ -47,6 +120,11 @@ void setup() {
   printf("Free heap size: %d\n", heap_caps_get_free_size(MALLOC_CAP_8BIT));
   printf("Total PSRAM size: %d\n", esp_psram_get_size());
   printf("Free PSRAM size: %d\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
+
+  //ESP-NOW
+  ESP_ERROR_CHECK(init_wifi());
+  ESP_ERROR_CHECK(init_esp_now());
+  ESP_ERROR_CHECK(register_peer(peer_mac));
 
   // Initialize model
   model = tflite::GetModel(g_person_detect_model_data);
@@ -199,6 +277,6 @@ void run_inference(void *ptr) {
   for (int i = 0; i < kCategoryCount; ++i) {
     sign_scores[i] = output->data.f[i];
   }
-  RespondToDetection(sign_scores, kCategoryLabels);
+  esp_now_send_data(peer_mac,RespondToDetection(sign_scores, kCategoryLabels), 32);
 }
 #endif
